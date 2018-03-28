8b8cea3f6d1ad7ed13acae6623a2c202b612e3cc
Pukisnew <dwhiszx@gmai.com>
2018-03-28 11:00:20 +0300

    first commit

---
+++ duombaze/AdminInfoLogin.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `AdminInfoLogin` (
+	`Nr`	INTEGER,
+	`Vardas`	TEXT,
+	`Pavardė`	TEXT,
+	`Elpaštas`	TEXT,
+	`Telefonas`	TEXT,
+	`Prisijungimas`	TEXT,
+	`Slaptažodis`	TEXT
+);
+INSERT INTO `AdminInfoLogin` (Nr,Vardas,Pavardė,Elpaštas,Telefonas,Prisijungimas,Slaptažodis) VALUES (1,'John','Smit','dwhiszx@gmail.com','860009251','admin','admin'),
+ (2,'Johan','Smit','pukisnew@usa.com','860009252','pukis','alus');
+COMMIT;
---
+++ duombaze/InfoLogin.sql
@@ -0,0 +1,16 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `InfoLogin` (
+	`Nr`	INTEGER,
+	`Vardas`	TEXT,
+	`Pavarde`	TEXT,
+	`Elpastas`	TEXT,
+	`Telefonas`	INTEGER,
+	`Login`	TEXT,
+	`Password`	TEXT,
+	`Data_Nuo`	INTEGER,
+	`Data_Iki`	TEXT
+);
+INSERT INTO `InfoLogin` (Nr,Vardas,Pavarde,Elpastas,Telefonas,Login,Password,Data_Nuo,Data_Iki) VALUES (1,'John','Smit','dwhiszx@gmail.com',860009251,'dwhiszx@gmail.com','123123','2018-01-01','2018-06-30'),
+ (2,'Johan','Smit','pukisnew@usa.com',860009252,'pukisnew@usa.com','0000','2018-01-01','2018-01-01'),
+ (7,'Jonas','Janas','j.janas@gmail.com',860009725,'j.janas@gmail.com','qwertas','2018-01-01','2018-02-30');
+COMMIT;
