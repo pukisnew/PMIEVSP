2999ab542cc55f2d06fba68ecd9afa2a75cda4d9
Mantas Gridinas <mgridinas@gmail.com>
2018-03-30 19:47:55 +0300

    resources

---
+++ src/i11/michalkevicius/deividas/controller/R.java
@@ -0,0 +1,15 @@
+package i11.michalkevicius.deividas.controller;
+
+public class R
+{
+    public static class FXML
+    {
+        public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
+        public static final String ADMIN_REG = "/fxml/adminreg.fxml";
+        public static final String LOGIN = "/fxml/login.fxml";
+        public static final String MENU_TWO = "/fxml/menutwo.fxml";
+        public static final String MENU_ONE = "/fxml/menuone.fxml";
+        public static final String USSERREG = "/fxml/usserreg.fxml";
+    }
+    //public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
+}
