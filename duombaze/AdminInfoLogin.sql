1e9519f9e27a8c359908c6ff2d8f9f27f4fe7f8b
Mantas Gridinas <mgridinas@gmail.com>
2018-03-30 20:23:23 +0300

    formatting

--- duombaze/AdminInfoLogin.sql
+++ duombaze/AdminInfoLogin.sql
@@ -1,13 +1,14 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS `AdminInfoLogin` (
-	`Nr`	INTEGER,
-	`Vardas`	TEXT,
-	`Pavardė`	TEXT,
-	`Elpaštas`	TEXT,
-	`Telefonas`	TEXT,
-	`Prisijungimas`	TEXT,
-	`Slaptažodis`	TEXT
+  `Nr`            INTEGER,
+  `Vardas`        TEXT,
+  `Pavardė`       TEXT,
+  `Elpaštas`      TEXT,
+  `Telefonas`     TEXT,
+  `Prisijungimas` TEXT,
+  `Slaptažodis`   TEXT
 );
-INSERT INTO `AdminInfoLogin` (Nr,Vardas,Pavardė,Elpaštas,Telefonas,Prisijungimas,Slaptažodis) VALUES (1,'John','Smit','dwhiszx@gmail.com','860009251','admin','admin'),
- (2,'Johan','Smit','pukisnew@usa.com','860009252','pukis','alus');
+INSERT INTO `AdminInfoLogin` (Nr, Vardas, Pavardė, Elpaštas, Telefonas, Prisijungimas, Slaptažodis)
+VALUES (1, 'John', 'Smit', 'dwhiszx@gmail.com', '860009251', 'admin', 'admin'),
+  (2, 'Johan', 'Smit', 'pukisnew@usa.com', '860009252', 'pukis', 'alus');
 COMMIT;
