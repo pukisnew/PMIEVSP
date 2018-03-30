587670f101657cb906024da92c82a965002bd0bd
Mantas Gridinas <mgridinas@gmail.com>
2018-03-30 22:45:17 +0300

    changes some resources

--- src/i11/michalkevicius/deividas/controller/R.java
+++ src/i11/michalkevicius/deividas/controller/R.java
@@ -7,9 +7,9 @@ public class R
         public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
         public static final String ADMIN_REG = "/fxml/adminreg.fxml";
         public static final String LOGIN = "/fxml/login.fxml";
-        public static final String MENU_TWO = "/fxml/menutwo.fxml";
+        public static final String ADMIN_PANEL = "/fxml/admin_panel.fxml";
         public static final String MENU_ONE = "/fxml/menuone.fxml";
-        public static final String USSERREG = "/fxml/usserreg.fxml";
+        public static final String USER_REG = "/fxml/user_reg.fxml";
     }
     //public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
 }
