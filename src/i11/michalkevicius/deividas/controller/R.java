e0e23c5d93d2ccaf5dec883185e0ed317e3d4caf
Mantas Gridinas <mgridinas@gmail.com>
2018-03-31 19:06:10 +0300

    adds resource

--- src/i11/michalkevicius/deividas/controller/R.java
+++ src/i11/michalkevicius/deividas/controller/R.java
@@ -7,6 +7,7 @@ public class R
         public static final String ADMIN_LOGIN = "/fxml/admin_login.fxml";
         public static final String ADMIN_PANEL = "/fxml/admin_panel.fxml";
         public static final String USER_REG = "/fxml/user_reg.fxml";
+        public static final String VIEWER_TABLE = "/fxml/lentele_perziurėti.fxml";
         public static final String MAIN = "/fxml/main.fxml";
     }
     //public static final String ADMIN_LOGIN = "/fxml/admin_login.fxml";
