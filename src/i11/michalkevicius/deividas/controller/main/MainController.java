1b74881bc1f48338e24c212bba0d6f3577165547
Mantas Gridinas <mgridinas@gmail.com>
2018-03-31 19:13:22 +0300

    launchers viewer stage

--- src/i11/michalkevicius/deividas/controller/main/MainController.java
+++ src/i11/michalkevicius/deividas/controller/main/MainController.java
@@ -1,9 +1,13 @@
 package i11.michalkevicius.deividas.controller.main;
 
+import i11.michalkevicius.deividas.controller.Database;
 import i11.michalkevicius.deividas.controller.SpreadsheetApp;
+import i11.michalkevicius.deividas.model.Product;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class MainController
 {
@@ -12,8 +16,10 @@ public class MainController
         SpreadsheetApp.launchAdminLoginStage();
     }
 
-    public void onDataTableViewClick(javafx.event.ActionEvent actionEvent)
+    public void onDataTableViewClick(javafx.event.ActionEvent actionEvent) throws SQLException, IOException
     {
+        List<Product> products = Database.getProducts();
+        SpreadsheetApp.launchViewerStage(products);
 
     }
 
