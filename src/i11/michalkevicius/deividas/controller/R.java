0fcfcbcc57eadba3f8cf09d43826c5dd67f2b7ce
Mantas Gridinas <mgridinas@gmail.com>
2018-03-31 16:05:40 +0300

    proper resource names

--- src/i11/michalkevicius/deividas/controller/R.java
+++ src/i11/michalkevicius/deividas/controller/R.java
@@ -4,12 +4,10 @@ public class R
 {
     public static class FXML
     {
-        public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
-        public static final String ADMIN_REG = "/fxml/adminreg.fxml";
-        public static final String LOGIN = "/fxml/login.fxml";
+        public static final String ADMIN_LOGIN = "/fxml/admin_login.fxml";
         public static final String ADMIN_PANEL = "/fxml/admin_panel.fxml";
-        public static final String MENU_ONE = "/fxml/menuone.fxml";
         public static final String USER_REG = "/fxml/user_reg.fxml";
+        public static final String MAIN = "/fxml/main.fxml";
     }
-    //public static final String ADMIN_LOGIN = "/fxml/adminlogin.fxml";
+    //public static final String ADMIN_LOGIN = "/fxml/admin_login.fxml";
 }
